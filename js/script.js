5be891d1075b670afd56c4905106889732c94428
Niko <36769202+Kanou38@users.noreply.github.com>
2022-09-23 10:37:25 +0200

    fix issue comment

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 let currentActiveMenu = "";
 
 $(document).ready(function() {
-    
+
     // startLoader();
-    
+
     // setTimeout(() => {
     //     stopLoader();
     // }, "3000")
-    
+
     initPage();
-    
+
     changeLottieOnHover();
-    
+
 });
 
 
@@ -19,13 +19,13 @@ $(document).ready(function() {
 const titles = document.querySelectorAll('.title-section--fixed');
 let titlePosition = {};
 
-titles.forEach( title => {        
-    titlePosition[title.id] = title.parentElement.offsetTop;         
- });
+titles.forEach(title => {
+    titlePosition[title.id] = title.parentElement.offsetTop;
+});
 
- console.log(titlePosition)
+console.log(titlePosition)
 
-window.onscroll = function() { 
+window.onscroll = function() {
 
     // let presentation = '#'+Object.keys(titlePosition)[0];    
     // console.log(presentation);
@@ -41,23 +41,22 @@ window.onscroll = function() {
 
 
     /**
-    * detect when the title get over the div
-    */
-    
-    let title = document.querySelector('.title-section--fixed'),
-    container = document.querySelector('#projets');
+     * detect when the title get over the div
+     */
 
-    si le scroll de la page depasse l element alors il deviens sticky
+    let title = document.querySelector('.title-section--fixed'),
+        container = document.querySelector('#projets');
+
+    // si le scroll de la page depasse l element alors il deviens sticky
     if (window.pageYOffset > title.offsetTop) {
         title.classList.add("sticky");
-    } 
+    }
 
 
-    if( window.pageYOffset  > (title.parentElement.offsetHeight - 400) + (title.parentElement.offsetHeight / 2) )
-    {
+    if (window.pageYOffset > (title.parentElement.offsetHeight - 400) + (title.parentElement.offsetHeight / 2)) {
         title.classList.remove("sticky");
     }
-   
+
 };
 
 
@@ -83,20 +82,20 @@ function changeLottieOnHover() {
 
 function startLoader() {
     var select = function(s) {
-        return document.querySelector(s);
-    },
-    selectAll = function(s) {
-        return document.querySelectorAll(s);
-    },
-    
-    animationWindow = select('#loader'),
-    anim = lottie.loadAnimation({
-        container: animationWindow,
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: "https://assets3.lottiefiles.com/packages/lf20_3w1whxz5.json"
-    });
+            return document.querySelector(s);
+        },
+        selectAll = function(s) {
+            return document.querySelectorAll(s);
+        },
+
+        animationWindow = select('#loader'),
+        anim = lottie.loadAnimation({
+            container: animationWindow,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: "https://assets3.lottiefiles.com/packages/lf20_3w1whxz5.json"
+        });
     document.getElementById('loader').style.display = "flex";
 }
 
@@ -106,23 +105,23 @@ function stopLoader() {
 
 function loadContentPage() {
     $('.js-menu ul li a').on('click', function(e) {
-        
+
         e.preventDefault();
-        
+
         if (currentActiveMenu !== e.currentTarget.dataset.link) {
-            
+
             // document.getElementById('loader').style.display = "flex";
-            
+
             $('.js-load__content').load('./partials/' + e.currentTarget.dataset.link + ".html", function(response, status, xhr) {
-                
+
                 if (status === 'success') {
                     document.getElementsByClassName('js-load__content').innerHTML = xhr.responseText;
                     // stopLoader();
                     $('body').scrollTo('#portfolio');
-                    
+
                 }
             });
-            
+
             currentActiveMenu = e.currentTarget.dataset.link;
         }
     });
@@ -143,11 +142,11 @@ function getHeader() {
 }
 
 function getFooter() {
-    
+
     $('.js-load__footer').load("./partials/footer.html", function(response, status, xhr) {
         if (status === "success") {
             document.getElementsByClassName('js-load__footer').innerHTML = xhr.responseText;
-            
+
         }
     });
 }
