01ae02547bdd3c22505b312428853dc44d3cf1c0
Niko <36769202+Kanou38@users.noreply.github.com>
2022-09-29 09:41:08 +0200

    integration des pages developpe

--- js/script.js
+++ js/script.js
@@ -1,29 +1,26 @@
-
-
-
 var cursor = document.querySelector('.cursor');
 var cursorinner = document.querySelector('.cursor2');
 var a = document.querySelectorAll('a');
 
-document.addEventListener('mousemove', function(e){
+document.addEventListener('mousemove', function(e) {
     var x = e.clientX;
     var y = e.clientY;
     cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
 });
 
-document.addEventListener('mousemove', function(e){
+document.addEventListener('mousemove', function(e) {
     var x = e.clientX;
     var y = e.clientY;
     cursorinner.style.left = x + 'px';
     cursorinner.style.top = y + 'px';
 });
 
-document.addEventListener('mousedown', function(){
+document.addEventListener('mousedown', function() {
     cursor.classList.add('click');
     cursorinner.classList.add('cursorinnerhover')
 });
 
-document.addEventListener('mouseup', function(){
+document.addEventListener('mouseup', function() {
     cursor.classList.remove('click')
     cursorinner.classList.remove('cursorinnerhover')
 });
@@ -38,36 +35,35 @@ a.forEach(item => {
 });
 
 $(document).ready(function() {
-    
+
     // startLoader();
-    
+
     // setTimeout(() => {
     //     stopLoader();
     // }, "3000")
-    
+
     // changeLottieOnHover();
-    
+
     let accordion_items = $('.accordion-item');
-    
+
     accordion_items.on('click', function(e) {
         $(this).closest('.row').toggleClass('h-0');
     });
-    
+
     $('.accordion-button').on('click', (e) => {
-        let idSection = $(e.target).closest('.accordion-item').find('.accordion-body');
-        
+        e.stopPropagation();
+        let idSection = $(e.target).closest('.accordion-item-home').find('.accordion-body');
+
         $([document.documentElement, document.body]).animate({
             scrollTop: $(idSection).offset().top,
         }, 800);
     });
-    
+
     $(".close__section").on('click', function() {
         $('.collapse').collapse('hide')
     });
-    
-    
-});
 
+});
 
 // const titles = document.querySelectorAll('.title-section--fixed');
 // let titlePosition = {};
@@ -87,7 +83,7 @@ $(document).ready(function() {
 //     selectAll = function(s) {
 //         return document.querySelectorAll(s);
 //     },
-    
+
 //     animationWindow = select('#loader'),
 //     anim = lottie.loadAnimation({
 //         container: animationWindow,
